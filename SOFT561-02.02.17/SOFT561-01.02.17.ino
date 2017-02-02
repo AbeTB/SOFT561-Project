e273536d5a0a341863a0dd9dbfa95a7fea354803
AbeTB <abraham.kolawole@postgrad.plymouth.ac.uk>
2017-02-02 16:00:51 +0000

    Add files via upload

---
+++ SOFT561-02.02.17/SOFT561-01.02.17.ino
@@ -0,0 +1,18 @@
+#include "motorControl.h"
+#include "ultrasound.h"
+
+ultrasound ultrasound;
+motorControl motorControl;
+long dist;
+
+void setup() {
+  // put your setup code here, to run once:
+motorControl.motorSetup();
+ultrasound.setupUS();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+motorControl.leftTurn(140);
+long dist = ultrasound.getDistLeft();
+}
