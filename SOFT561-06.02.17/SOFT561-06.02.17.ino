7ea6b647fa55ba8d0eab448367b95b4f908277b2
AbeTB <abraham.kolawole@postgrad.plymouth.ac.uk>
2017-02-06 16:20:03 +0000

    Add files via upload

---
+++ SOFT561-06.02.17/SOFT561-06.02.17.ino
@@ -0,0 +1,20 @@
+#include "motorControl.h"
+#include "ultrasound.h"
+
+int usSpeed = dist * 3;
+ultrasound ultrasound;
+motorControl motorControl;
+long dist;
+
+void setup() {
+  // put your setup code here, to run once:
+motorControl.motorSetup();
+ultrasound.setupUS();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+long dist = ultrasound.getDistLeft();
+motorControl.forwardDrive(usSpeed);
+
+}
---
+++ SOFT561-06.02.17/motorControl/motorControl.cpp
@@ -0,0 +1,60 @@
+#include "motorControl.h"
+
+const int pwmA  = 3;
+const int pwmB  = 11;
+const int brakeA  = 9;
+const int brakeB  = 8;
+int lSpeed;
+int rSpeed;
+int dSpeed;
+
+
+void motorControl::motorSetup()
+{
+  pinMode(pwmA, OUTPUT);
+  pinMode(pwmB, OUTPUT);
+  pinMode(brakeA, OUTPUT);
+  pinMode(brakeB, OUTPUT);
+}
+
+void motorControl::steerAngle()
+{
+  digitalWrite(6, HIGH);
+  delay(250);
+  digitalWrite(6, LOW);
+  delay(250);  
+}
+
+void motorControl::rightTurn(int lSpeed)
+{
+  digitalWrite(brakeA, HIGH);
+  digitalWrite(brakeB, LOW);
+  analogWrite(pwmA, 0);
+  analogWrite(pwmB, lSpeed); 
+}
+
+void motorControl::leftTurn(int rSpeed)
+{
+  digitalWrite(brakeA, LOW);
+  digitalWrite(brakeB, HIGH);
+  analogWrite(pwmA, rSpeed);
+  analogWrite(pwmB, 0); 
+}
+
+void motorControl::brake()
+{
+  digitalWrite(brakeA, HIGH);
+  digitalWrite(brakeB, HIGH);
+  analogWrite(pwmA, 0);
+  analogWrite(pwmB, 0); 
+}
+
+void motorControl::forwardDrive(int dSpeed)
+{
+  digitalWrite(brakeA, LOW);
+  digitalWrite(brakeB, LOW);
+  analogWrite(pwmA, dSpeed);
+  analogWrite(pwmB, dSpeed); 
+}
+
+
